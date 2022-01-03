579cfb513c24a972fc55140cd30adff90ab67bd3
sai vardhan <85542869+vardhankadambala@users.noreply.github.com>
2022-01-03 23:09:00 +0530

    Add files via upload

---
+++ Calc/src/Calc.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Calc 
+{
+    public static void main(String[] args)
+    {
+        Calculator calc = new Calculator();
+        calc.show();
+        
+    } 
+            
+    
+}
